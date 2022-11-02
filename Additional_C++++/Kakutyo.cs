9b4ad4012392941515ab60e72e7e2f491bf97c94
MIZUKI\mizuk <m-otomo@powersolutions.co.jp>
2022-11-02 19:13:53 +0900

    2022/11/2

---
+++ Additional_C++++/Kakutyo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Additional_C____
+{
+    public static class Kakutyo
+    {
+        //既存のクラスなどの型に対して、追加のインスタンスメソッドを定義できる（ように見える）機能
+        //staticクラスのstaticメソッドで、最初の引数を以下のようにする
+        //・thisを付与する
+        //・引数の型を、拡張メソッドを追加する対象の型にする
+        //対象の型を、拡張メソッドを定義しているクラスが定義されている名前空間をusingすることで、拡張メソッドが有効になる
+        public static int ParseInt(this string self)
+        {
+            return int.Parse(self);
+        }
+        //"123456".ParseInt();//123456(int)
+        //実際には、対応するstaticメソッドの呼び出しがされているだけ
+        //なお、拡張メソッドは普通のstaticメソッドの発展版なので、、下記のように呼び出すこともできる
+
+        //Kakutyo.ParseInt("123456");
+
+        //Q.同一ファイル内であればおそらくクラス名は不要
+        //Q.同じファイル内に書いていればusingは必要ない？
+
+    }
+}
---
+++ Additional_C++++/Seiteki.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Additional_C____
+{
+    public static class Seiteki
+    {
+        //すべてのメンバが、クラスメンバ（staticメンバ・静的メンバ）であるクラス
+        //staticをクラス宣言部に付与することで、下記の特性を持つクラスとなる
+        
+        //static class XXX{ /*クラスメンバ定義*/ }
+        
+        //---特徴---
+        //・クラスメンバのみ宣言可能、インスタンスメンバは宣言不可
+        //・インスタンス化ができない
+        //・継承不可（親はobjectクラス限定）
+        
+        //標準で用意されているMathクラスのような、
+        //特定の問題領域に対する処理を行うためのメソッド・プロパティ・定数などを提供することを
+        //目的としたクラス（ユーティリティクラス）をstaticクラスにすることが多い
+
+    }
+}
